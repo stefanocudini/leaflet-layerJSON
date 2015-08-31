41cf6315cd4eb6e3e9d9c056a0e13f6c16131a2f
stefanocudini <stefano.cudini@gmail.com>
2015-08-31 03:01:06 +0200

    update pkg .jg

--- package.js
+++ package.js
@@ -1,20 +1,8 @@
 Package.describe({
-	name: 'leaflet-layerjson',
+	name: 'stefcud:leaflet-layerjson',
 	version: '0.1.7',
-	summary: "Leaflet Dynamic JSON Layer"
-});
-
-Package.on_use(function (api, where) {
-	api.add_files('dist/leaflet-layerjson.min.js', 'client');
-	//TODO server-side methods...	
-});
-
-Package.describe({
-	summary: "Leaflet Control plugin for tracking gps position, with more options",
-	name: "stefcud:leaflet-gps",
-	version: "1.0.2",
-	summary: "Leaflet Control GPS",
-	git: "https://github.com/stefanocudini/leaflet-gps.git"
+	summary: "Leaflet Dynamic JSON Layer",
+	git: "https://github.com/stefanocudini/leaflet-layerjson.git"	
 });
 
 Package.on_use(function (api, where) {
