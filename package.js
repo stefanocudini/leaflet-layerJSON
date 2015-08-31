eea1da8ecb196dc9d1cfd7b051c0ab50813d8a2b
stefanocudini <stefano.cudini@gmail.com>
2015-08-31 03:00:27 +0200

    update pkg .jg

--- package.js
+++ package.js
@@ -8,3 +8,15 @@ Package.on_use(function (api, where) {
 	api.add_files('dist/leaflet-layerjson.min.js', 'client');
 	//TODO server-side methods...	
 });
+
+Package.describe({
+	summary: "Leaflet Control plugin for tracking gps position, with more options",
+	name: "stefcud:leaflet-gps",
+	version: "1.0.2",
+	summary: "Leaflet Control GPS",
+	git: "https://github.com/stefanocudini/leaflet-gps.git"
+});
+
+Package.on_use(function (api, where) {
+	api.addFiles('dist/leaflet-layerjson.min.js', 'client');
+});
