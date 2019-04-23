950b567eceaa45e85acd0f33b3da85e7852a297d
stefanocudini <stefano.cudini@gmail.com>
2019-04-23 17:13:49 +0200

    update version

--- dist/leaflet-layerjson.src.js
+++ dist/leaflet-layerjson.src.js
@@ -1,7 +1,7 @@
 /* 
- * Leaflet JSON Layer v0.2.8 - 2017-08-11 
+ * Leaflet JSON Layer v0.3.0 - 2019-04-23 
  * 
- * Copyright 2017 Stefano Cudini 
+ * Copyright 2019 Stefano Cudini 
  * stefano.cudini@gmail.com 
  * http://labs.easyblog.it/ 
  * 
@@ -134,12 +134,26 @@ L.LayerJSON = L.FeatureGroup.extend({
 
 	clearLayers: function () {
 
-		this._markersCache = {};	//cached gen markers
+		var self = this,
+			newBounds = this._map.getBounds();
+		
+		self._markersCache = {};	//cached gen markers
 
-		if(this.options.layerTarget)
-			this.options.layerTarget.clearLayers.call(this.options.layerTarget);
-		else
-			L.FeatureGroup.prototype.clearLayers.call(this);
+		if(self.options.layerTarget) {
+			//self.options.layerTarget.clearLayers.call(self.options.layerTarget);
+			self.options.layerTarget.eachLayer(function(l) {
+				if(!self._contains(newBounds, l) )
+					self.options.layerTarget.removeLayer(l);
+			});
+		}
+		else {
+			//L.FeatureGroup.prototype.clearLayers.call(self);
+			self.eachLayer(function(l) {
+				if(!self._contains(newBounds, l) )
+					self.removeLayer(l);
+			}, self);
+		}
+
 		return this;
 	},
 	
